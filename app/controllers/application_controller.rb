9dc9b86018abc18e5f13545e8088903ec7e3ef7e
Rebecca Strauss <rstrauss127@gmail.com>
2019-02-12 16:30:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,32 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "carcollection"
   end
 
+  get '/' do
+    "Welcome to Fwitter"
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+
+    if params[:username].empty?
+      redirect '/signup'
+    elsif params[:email].empty?
+      redirect '/signup'
+    elsif params[:password].empty?
+      redirect '/signup'
+    else
+      redirect '/tweets'
+    end
+  end
+
+  get '/tweets' do
+
+  end
 end
